7130b49e7f565de28dc03e7eaaa0c0fa31ed62a0
reiczel01 <reiczel.01@gmail.com>
2021-01-30 09:28:54 +0100

    v2

---
+++ 1.26/1.26/1.26.c
@@ -0,0 +1,11 @@
+﻿
+#include <stdio.h>
+
+int main()
+{
+	int day, month, year;
+	printf("Podaj datę:");
+	scanf("%02d-%02d-%04d", &day, &month, &year);
+	printf("Day:%*02d\nMontch:\t%02d\nYear:\t%04d", day, month, year);
+	return 0;
+}
