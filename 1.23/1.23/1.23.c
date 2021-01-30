21fe38f9f9319a8e1076a410ecee1aad8bdefe03
reiczel01 <reiczel.01@gmail.com>
2021-01-30 08:38:19 +0100

    v1

---
+++ 1.23/1.23/1.23.c
@@ -0,0 +1,11 @@
+﻿
+#include <stdio.h>
+
+int main()
+{
+	int num, num2, num3;
+	printf("Podaj liczby:");
+	scanf("%03d%03d%03d", &num, &num2, &num3);
+	printf("%03d\n%03d\n%03d", num, num2, num3);
+	return 0;
+}
